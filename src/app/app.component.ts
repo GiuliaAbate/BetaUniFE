dc4d8c04f24124532575f417eae4b3374f587817
Giulia Abate <giulia.abate1101@gmail.com>
2025-07-27 17:32:44 +0200

    footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './features/navbar/navbar.component';
 import { FormsModule } from '@angular/forms';
+import { FooterComponent } from './features/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,NavbarComponent,FormsModule],
+  imports: [RouterOutlet,NavbarComponent,FormsModule,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
